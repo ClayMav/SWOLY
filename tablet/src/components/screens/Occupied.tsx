3f52f9c4f324faca97091a206c0e06954fb99ef1
Clay McGinnis <mcginnis@claymav.com>
2019-10-29 15:16:28 -0500

    Update from database when exercise in progress

--- tablet/src/components/screens/Occupied.tsx
+++ tablet/src/components/screens/Occupied.tsx
@@ -1,73 +1,106 @@
-import React from 'react';
-import {Query} from 'react-apollo';
-import gql from 'graphql-tag';
-import {useQuery} from '@apollo/react-hooks'
-import { userInfo } from 'os';
+import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
-// const GET_GYMS = gql`
-//   {
-//     gyms {
-//       id
-//       name
-//       address
-//       location {
-//         latitude
-//         longitude
-//       }
-//     }
-//   }
-// `;
+import styled from "styled-components";
 
 const GET_WORKOUT = gql`
-{
-    workout(id: "z4CsysmySGGT0AsblAg8") {
-        name
-        createdBy {
+  query getUser($id: String!) {
+    user(id: $id) {
+      workouts {
+        inProgress
+        workout {
+          name
+          createdBy {
             name
-        }
-        exercises {
+          }
+          exercises {
             exercise {
-                name
-                instructions
+              name
+              instructions
             }
             reps
             sets
             weight
+          }
         }
+      }
     }
-}      
+  }
+`;
+const GET_MST_GYM = gql`
+  {
+    gym(id: "Ol0462y0TQTIv22TPAUA") {
+      stations {
+        occupiedBy {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const Header = styled.div`
+  height: 100px;
+`;
+const Heading = styled.h1``;
+
+const ImgHolder = styled.div`
+  width: 40%;
+`;
+const Wrap = styled.div`
+  display: flex;
+`;
+const Info = styled.div`
+  font-size: 20px;
+  flex: 1;
+`;
+const Img = styled.img`
+  width: 100%;
 `;
 
 export default function Occupied() {
-    const { loading, error, data } = useQuery(GET_WORKOUT);
-    if(loading)
-        return <p>'Loading'</p>;
-    if(error)
-        return <p>'error'</p>;
-    console.log(data.exercises);
+  const { loading: gloading, data: gdata } = useQuery(GET_MST_GYM);
+  const { loading, error, data } = useQuery(GET_WORKOUT, {
+    skip: gloading,
+    variables: { id: gdata ? gdata.gym.stations[0].occupiedBy.id : null }
+  });
+  if (gloading || loading) return <p>'Loading'</p>;
+  if (error) return <p>'error'</p>;
+  const workout = data.user.workouts[0];
+  console.log(workout);
 
-    function abort() {
-        alert("This stations has been aborted.");
-        // Redirect to unoccupied here
-    }
-    function complete() {
-        alert("Your exercise is now complete");
-    }
-    return(
-        <div>
-            <h1>Hello {data.workout.createdBy.name}!</h1>
-            <h2>Your workout today is: {data.workout.name}</h2>
-            <h3>Current exercise: {data.workout.exercises[0].exercise.name}</h3>
-            <img src={data.workout.exercises[0].exercise.instructions} />
-            <ul>
-                <li>Sets: {data.workout.exercises[0].reps}</li>
-                <li>Reps: {data.workout.exercises[0].sets}</li>
-                <li>Weight: {data.workout.exercises[0].weight}lbs</li>
-            </ul>
-            <button 
-                onClick={abort}>Abort
-            </button>
-            <button onClick={complete}>Done</button>
-        </div>
-    )
+  function abort() {
+    alert("This stations has been aborted.");
+    // Redirect to unoccupied here
+  }
+  function complete() {
+    alert("Your exercise is now complete");
+  }
+  return (
+    <div>
+      <Header>
+        <Heading>Hey, {workout.workout.createdBy.name}!</Heading>
+        <h2>Your workout today is: {workout.workout.name}</h2>
+      </Header>
+      <Wrap>
+        <ImgHolder>
+          <Img src={workout.workout.exercises[0].exercise.instructions} />
+        </ImgHolder>
+        <Info>
+          <h3>
+            Current exercise: {workout.workout.exercises[0].exercise.name}
+          </h3>
+          <ul>
+            <li>Sets: {workout.workout.exercises[0].reps}</li>
+            <li>Reps: {workout.workout.exercises[0].sets}</li>
+            <li>Weight: {workout.workout.exercises[0].weight}lbs</li>
+          </ul>
+          <button onClick={abort}>Abort</button>
+          <button onClick={complete}>Done</button>
+        </Info>
+      </Wrap>
+    </div>
+  );
 }
+
--- tablet/src/components/screens/Unoccupied.tsx
+++ tablet/src/components/screens/Unoccupied.tsx
@@ -1,19 +1,49 @@
-import React from 'react'
+import React from "react";
 
+import { Redirect } from "react-router-dom";
+
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const GET_MST_GYM = gql`
+  {
+    gym(id: "Ol0462y0TQTIv22TPAUA") {
+      stations {
+        occupiedBy {
+          id
+        }
+      }
+    }
+  }
+`;
 
 export default function Unoccupied(this: any) {
-    function occupyStation () {
-        alert("The station is now occupied")
-        // Redirec to occupied, here
-    }
-    return (
-        <div>
-            <h1>Hello from the unoccupied view</h1>
-            <h2>This is screen is for showing the user/gym that the current station is empty</h2>
-            <div>
-            <button 
-                onClick={occupyStation}>Occupy</button>
-            </div>
-        </div>
-    )
-};
+  const { data } = useQuery(GET_MST_GYM, {
+    pollInterval: 200
+  });
+
+  if (data) {
+    console.log(data);
+  }
+
+  if (data && data.gym.stations[0].occupiedBy) {
+    return <Redirect to="/occupied" />;
+  }
+
+  function occupyStation() {
+    alert("The station is now occupied");
+    // Redirec to occupied, here
+  }
+  return (
+    <div>
+      <h1>Hello from the unoccupied view</h1>
+      <h2>
+        This is screen is for showing the user/gym that the current station is
+        empty
+      </h2>
+      <div>
+        <button onClick={occupyStation}>Occupy</button>
+      </div>
+    </div>
+  );
+}
